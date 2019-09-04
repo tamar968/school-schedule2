7234b9ab8206cecfcac3b69c9170a5fae4bf7ec1
hitmachut <hitmachut@students.local>
2019-09-04 21:22:35 +0300

    typetype

--- TestCode/Program.cs
+++ TestCode/Program.cs
@@ -14,25 +14,25 @@ namespace TestCode
         {
             var xmlManager = new XmlManager();
 
-            //xmlManager.LoadDirectory(@"Y:\group 2 תשעט\הכהן עדני תמר\קבצים לפרויקט\תלמש\Talmash\חשון עט\");
-            //xmlManager.LoadDirectory("../../XML_files");
-            //var l = Teacher.Get();
-            //foreach (var item in l)
-            //{
-            //    Console.WriteLine(item);
-            //}
-            // LogManager.LogException(new Exception("be carefull"));
-            BL.AbsenceForTeacher.Add(new AbsencesForTeacherDTO()
+            xmlManager.LoadDirectory(@"Y:\group 2 תשעט\הכהן עדני תמר\קבצים לפרויקט\תלמש\Talmash\חשון עט\");
+            xmlManager.LoadDirectory("../../XML_files");
+            var l = Teacher.Get();
+            foreach (var item in l)
             {
-                FromDate = _CastDTO.DateToDTO(DateTime.Now),
-                ToDate = _CastDTO.DateToDTO(DateTime.Now),
-                TeacherId = 179,
-                Type = 4,
-                TeacherStandIn = 180
-            });
-            var z = BL.Class.ClassByLayer.Keys;
-            var x = BL.Class.ClassByLayer;
-            Console.ReadLine();
+                Console.WriteLine(item);
+            }
+            LogManager.LogException(new Exception("be carefull"));
+            //BL.AbsenceForTeacher.Add(new AbsencesForTeacherDTO()
+            //{
+            //    FromDate = _CastDTO.DateToDTO(DateTime.Now),
+            //    ToDate = _CastDTO.DateToDTO(DateTime.Now),
+            //    TeacherId = 179,
+            //    Type = 4,
+            //    TeacherStandIn = 180
+            //});
+            //var z = BL.Class.ClassByLayer.Keys;
+            //var x = BL.Class.ClassByLayer;
+            //Console.ReadLine();
         }
     }
 }
