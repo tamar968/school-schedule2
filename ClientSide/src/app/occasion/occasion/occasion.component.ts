57a381de2471844ddffd23b11a39213200146b5a
hitmachut <hitmachut@students.local>
2019-09-15 11:14:18 +0300

    del occation fill list occasions

--- ClientSide/src/app/occasion/occasion/occasion.component.ts
+++ ClientSide/src/app/occasion/occasion/occasion.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { OccasionService } from 'src/app/services/occasion.service';
+import { Occasion } from 'src/app/models/occasion.model';
 
 @Component({
   selector: 'app-occasion',
@@ -8,9 +10,14 @@ import { Router } from '@angular/router';
 })
 export class OccasionComponent implements OnInit {
 
-  constructor(public router: Router) { }
+  constructor(public router: Router,private occService:OccasionService) { }
+  occasions:Occasion[];
 
   ngOnInit() {
+    this.occService.getall()
+      .subscribe(occasions => {
+        this.occasions = occasions;
+      },err=>console.error(err))
   }
   navigateToAdd() {
     this.router.navigate(['/occasion/add']);
--- ClientSide/src/app/occasion/view-occasion/view-occasion.component.ts
+++ ClientSide/src/app/occasion/view-occasion/view-occasion.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 
+
 @Component({
   selector: 'app-view-occasion',
   templateUrl: './view-occasion.component.html',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ViewOccasionComponent implements OnInit {
 
-  constructor() { }
-
+  constructor( ) { }
   ngOnInit() {
+    
   }
 
 }
