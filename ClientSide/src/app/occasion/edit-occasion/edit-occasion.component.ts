6285997fb7f19a7e989de10d514122142d6e97ed
hitmachut <hitmachut@students.local>
2019-09-15 15:57:32 +0300

    in the middle edit-occ

--- ClientSide/src/app/occasion/edit-occasion/edit-occasion.component.ts
+++ ClientSide/src/app/occasion/edit-occasion/edit-occasion.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { Occasion } from 'src/app/models/occasion.model';
 import { OccasionService } from 'src/app/services/occasion.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { OccasionTypeService } from 'src/app/services/occasion-type.service';
+import { OccasionType } from 'src/app/models/occasion-type.model';
 
 @Component({
   selector: 'app-edit-occasion',
@@ -10,22 +12,31 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class EditOccasionComponent implements OnInit {
 
-  constructor(private activatedRoute:ActivatedRoute,private router:Router, private occService:OccasionService) { }
+  constructor(private activatedRoute: ActivatedRoute, private router: Router, private occService: OccasionService, private occTypeService: OccasionTypeService) { }
 
-occ:Occasion;
+  occ: Occasion;
+  occTypes: OccasionType[];
+
   ngOnInit() {
     this.occService.get(Number(this.activatedRoute.snapshot.params['number']))
-    .subscribe(occ => {
-      this.occ = occ;
-      console.log(occ);
-    },err=>console.error(err))
+      .subscribe(occ => {
+        this.occ = occ;
+        console.log(occ); this.occTypeService.getOccasionTypes()
+          .subscribe(occasionTypes => {
+            this.occTypes = occasionTypes;
+            this.occ.OccasionTypeName = this.occTypes.filter(ot => ot.Id === this.occ.OccasionType)[0].Name;
+          },
+            err => console.error(err)
+          );
+      }, err => console.error(err))
+
   }
-  delete(id:number){
+  delete(id: number) {
     this.occService.delete(id)
-    .subscribe(res => {
-     console.log(`נמחק בהצלחה ${res}`);
-     this.router.navigateByUrl('');
-    },err=>console.error(err))
+      .subscribe(res => {
+        console.log(`נמחק בהצלחה ${res}`);
+        this.router.navigateByUrl('');
+      }, err => console.error(err))
   }
   navigateToList() {
     this.router.navigate([`/occasion/occasion`]);
